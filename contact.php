99fda45994c607ea01ad3551f10a959099a5e8ea
azizullahsaeidi <azizullahsaeidi@gmail.com>
2019-07-21 17:40:33 +0430

    new changes

--- contact.php
+++ contact.php
@@ -6,5 +6,8 @@
 <body>
 <h1>How are you dear MDEZ</h1>
 <div>This is new insertation</div>
+<div>This is new insertation</div>
+<div>This is new insertation</div>
+<div>This is new insertation</div>
 </body>
 </html>
